769b4e8e435d41fbb725bdb5eded0dbc0cdb2feb
valttepe <valtteri.pesu@hotmail.com>
2018-04-25 18:05:59 +0300

    trying out google maps nearby search for football fields

--- public/javascripts/map-client.js
+++ public/javascripts/map-client.js
@@ -1,17 +1,42 @@
-const initMap = () =>{
-    let myLatLng = {
-      lat: 43.6222102,
-      lng: -79.6694881,
-    };
+let map;
+let infowindow;
 
-    let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
-      center: myLatLng,
-    });
+const initMap = () => {
+  let pyrmont = {lat: 60.218208, lng: 24.811968};
 
-    let marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-    });
-  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 15,
+  });
+
+  infowindow = new google.maps.InfoWindow();
+  let service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: pyrmont,
+    radius: 5000,
+    keyword: ['jalkapallokenttä'],
+  }, callback);
+}
+
+const callback = (results, status) => {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+        console.log(results[i]);
+        createMarker(results[i]);
+    }
+  }
+}
+
+const createMarker = (place) => {
+  let placeLoc = place.geometry.location;
+  let marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, 'click', () => {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
   google.maps.event.addDomListener(window, 'load', initMap);
