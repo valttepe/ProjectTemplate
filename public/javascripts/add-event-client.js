df17dddcb28f23b3c31b3a67c110254d21ba51ce
valttepe <valtteri.pesu@hotmail.com>
2018-05-04 10:36:08 +0300

    Slider and rest of the input fields added and form UI is improved

--- public/javascripts/add-event-client.js
+++ public/javascripts/add-event-client.js
@@ -1,1 +1,19 @@
+'use strict';
 
+// Datetime picker function
+$(() => {
+    $('#datetimepicker1').datetimepicker();
+    });
+
+// Range slider
+
+const slider = document.querySelector('.slider');
+const output = document.querySelector('.value');
+console.log(slider);
+console.log(output);
+output.innerHTML = 'Count: ' + slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = () => {
+    output.innerHTML = 'Count: ' + slider.value;
+};
